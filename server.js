dcad71e4f4a27b1050498d45ac32ea6a62cdebd5
Sree Ranganath <skoonisetti@byupathway.edu>
2025-07-06 04:25:32 +0530

    Cleaned up server.js and configured EJS layout

--- server.js
+++ server.js
@@ -7,9 +7,12 @@
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
+const dotenv = require("dotenv").config()
+const static = require("./routes/static")
+
+/* App Initialization*/
 
 const app = express()
-/* Routes*/
 
 
 /* View Engine and Templates */
@@ -17,27 +20,30 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
+
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
+
+
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on http://${host}:${port}`)
 })
 
 //Index route
