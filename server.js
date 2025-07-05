345cdb6ad3cfe12803afa5bd90a0159231c389cf
Sree Ranganath <skoonisetti@byupathway.edu>
2025-07-06 04:03:29 +0530

    Set up EJS layout and partials for homepage

--- server.js
+++ server.js
@@ -6,6 +6,15 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+/* Routes*/
+
+
+/* View Engine and Templates */
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
