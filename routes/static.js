16289141c2ea673061b2b7a3967c4d8f7589e796
Sree Ranganath <skoonisetti@byupathway.edu>
2025-07-06 17:41:31 +0530

    updates

--- routes/static.js
+++ routes/static.js
@@ -8,6 +8,31 @@ router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
+router.get("/custom", (req, res) => {
+  res.send("<h2>Custom page coming soon!</h2>")
+})
+
+router.get("/sedan", (req, res) => {
+  res.send("<h2>Sedan page coming soon!</h2>")
+})
+
+router.get("/suv", (req, res) => {
+  res.send("<h2>SUV page coming soon!</h2>")
+})
+
+router.get("/truck", (req, res) => {
+  res.send("<h2>Truck page coming soon!</h2>")
+})
+
+router.get("/inventory", (req, res) => {
+  res.render("inventory", { title: "Inventory" })
+})
+
+router.get("/account/login", (req, res) => {
+  res.render("login", { title: "Login" })
+})
+
+
 module.exports = router;
 
 
